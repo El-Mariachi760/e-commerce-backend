c2942637da400f7ca5031e9ba61a7ed07effabd5
Pancho Villa <94568874+El-Mariachi760@users.noreply.github.com>
2022-07-22 16:07:08 -0700

    created the Category object and routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,25 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  console.log('======================');
+  Category.findAll({
+      attributes: ['id', 
+      'category_name'
+  ],
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+  })
+
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 router.get('/:id', (req, res) => {
@@ -15,14 +34,58 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbUserData => res.json(dbUserData))
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
+// PUT /api/categories/1
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    individualHooks: true,
+    where: {
+        id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+      if (!dbUserData[0]) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+      }
+      res.json(dbUserData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
+
+// DELETE /api/categories/1
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+    }
+  })
+  .then(dbUserData => {
+      if (!dbUserData) {
+      res.status(404).json({ message: 'No category found with this id' });
+      return;
+      }
+      res.json(dbUserData);
+  })
+  .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 module.exports = router;
